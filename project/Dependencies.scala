626372175fff620377e9b98eafc2ac291d7df12b
Miguel Villafuerte <mivimur@hotmail.com>
2020-05-19 14:56:25 +0200

    chore(Dependencies) : update zio artifacts version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Dependencies {
     val plantUml                  = "1.2020.10"
     val scalaTest                 = "3.1.2"
     val zio                       = "1.0.0-RC19-1"
-    val zioLogging                = "0.2.8"
+    val zioLogging                = "0.2.9"
   }
 
 }
