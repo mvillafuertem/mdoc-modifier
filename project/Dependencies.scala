292b9bbd6b97c92b55922b4af30a14d7f65ed034
Scala Steward <me@scala-steward.org>
2020-08-05 02:25:16 +0200

    Update zio-logging-slf4j to 0.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,7 +45,7 @@ object Dependencies {
     val plantUml                  = "1.2020.15"
     val scalaTest                 = "3.2.0"
     val zio                       = "1.0.0-RC21-2"
-    val zioLogging                = "0.3.2"
+    val zioLogging                = "0.4.0"
   }
 
 }
