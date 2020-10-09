290258aca90a36f3edc9d2f659fbc16ce218a7ae
Scala Steward <me@scala-steward.org>
2020-10-09 02:32:39 +0200

    Update zio, zio-streams, zio-test, ... to 1.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
     val akka                      = "2.6.9"
     val plantUml                  = "1.2020.15"
     val scalaTest                 = "3.2.2"
-    val zio                       = "1.0.1"
+    val zio                       = "1.0.3"
     val zioLogging                = "0.5.1"
   }
 
