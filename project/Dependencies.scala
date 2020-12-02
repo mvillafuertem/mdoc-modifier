957eda77ad528df4eb4bfc2edb38d3cc9e3baf03
Scala Steward <me@scala-steward.org>
2020-12-02 02:46:05 +0100

    Update plantuml to 1.2020.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
   private object Version {
     val akkaHttp                  = "10.2.0"
     val akka                      = "2.6.9"
-    val plantUml                  = "1.2020.15"
+    val plantUml                  = "1.2020.21"
     val scalaTest                 = "3.2.2"
     val zio                       = "1.0.1"
     val zioLogging                = "0.5.1"
